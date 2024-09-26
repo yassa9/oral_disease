36fa95c445794847e39c378287367d67b21b705f
MateBerg <yassadaw9@gmail.com>
2024-09-26 14:12:07 +0300

    update segmentor.py - .pkl

--- Supervised_fashon/First_approach/segmentor.py
+++ Supervised_fashon/First_approach/segmentor.py
@@ -7,13 +7,7 @@ import requests
 import os
 import random
 import time
-from base_model import device
-from config import (
-    full_train_data_path,
-    full_val_data_path,
-    full_test_data_path,
-    full_masks_data_path,
-)
+import pickle
 
 generator = pipeline(
     "mask-generation",
@@ -27,7 +21,7 @@ def derive_mask_path(image_path):
     mask_path = image_path.replace('/splitted/', '/masks/')
     
     # Change file extension to '.npy'
-    mask_path = os.path.splitext(mask_path)[0] + '.npy'
+    mask_path = os.path.splitext(mask_path)[0] + '.pkl'
     return mask_path
 
 
@@ -36,13 +30,15 @@ def process_image(image_path, show_time=True):
     mask_path = derive_mask_path(image_path)
     if os.path.exists(mask_path):
         print(f"Mask already exists for {image_path}, skipping.")
-        masks = np.load(mask_path, allow_pickle=True)
+        with open(mask_path, 'rb') as f:
+            masks = pickle.load(f)
         return masks
     else:
         print(f"Generating mask for {image_path}")
         image = Image.open(image_path).convert('RGB')
         masks = generator(image)
-        np.save(mask_path, masks)
+        with open(mask_path, 'wb') as f:
+            pickle.dump(masks, f)
         print(f"Mask saved at {mask_path}")
         end_time = time.time()
         if show_time:
@@ -51,7 +47,7 @@ def process_image(image_path, show_time=True):
             seconds = total_time % 60
             print(f"Time taken: {minutes} minutes and {seconds:.2f} seconds")
         return masks
-        
+                
 
 def show_mask(mask, ax, random_color=False):
     if random_color:
